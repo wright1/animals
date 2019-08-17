ff709a2a135550708b2c5865bf439cb69b967a5f
wright1 <prosperwright@yahoo.co.uk>
2019-08-17 13:21:39 +0100

    first iteration of button

---
+++ src/components/game/button.js
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components"
+
+const Choice = styled.button`
+width: 15vw;
+height: 10vh;
+border: solid 0.3em red;
+border-radius: 5px;
+`
+
+const Button = ({ text }) => {
+    return(
+        <Choice>{ text }</Choice>
+    )
+}
+
+export default Button
--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,9 +1,14 @@
 import React from "react"
+import Button from "./button"
 
 
 const Game = () =>{
     return(
-        <div><h1>This is the game page</h1></div>
+        <div>
+            <h1>This is the game page</h1>
+            <Button text="Cow" />
+            
+            </div>
     )
 }
 
